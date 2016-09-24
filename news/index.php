53af1dee0a51856d91e014448cf69b43525ba783
dmdoa12 <dmdoa12@gmail.com>
2016-09-25 03:10:59 +0900

    php basic layout (Proceeding) and survey two site , ubuntu setting(Proceeding)

---
+++ news/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+<style type="text/css">
+<!--
+html,body{
+	height:100%;
+	overflow:hidden;
+}
+#wrapper {
+	max-width:auto;
+	margin: 0 auto;
+	height:100%;
+	-webkit-box-sizing: border-box;
+	-moz-box-sizing: border-box;
+	box-sizing: border-box;
+}
+
+#header {
+	margin: 0 auto;
+	height: 20%;
+	background-color: gray;
+}
+
+#content {
+	margin: 0 auto;
+	height: 80%;
+	background-image:url("image/mainImage.jpg");
+	background-size:cover;
+}
+
+.fixed {
+	position: fixed;
+	bottom: 0;
+	right: 0;
+	width: 200px;
+	background-color: white;
+}
+//
+-->
+</style>
+</head>
+<body>
+	<div id="wrapper">
+		<div id="header">header</div>
+		<div id="content">content</div>
+	</div>
+</body>
+</html>
