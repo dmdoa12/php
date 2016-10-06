25601914b5a8f07eb1c9568fd934bf8f3a88c32f
dmdoa12 <dmdoa12@gmail.com>
2016-10-06 22:46:24 +0900

    login_session 기능 추가

--- application/views/_footer.php
+++ application/views/_footer.php
@@ -1,4 +1,3 @@
-			  </div>
 		</div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="/asset/lib/bootstrap/js/bootstrap.min.js"></script>
--- application/views/_head.php
+++ application/views/_head.php
@@ -5,10 +5,37 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>뉴스 토론 게시판</title>
+	<link href="/asset/css/main.css" rel="stylesheet">	
 	<link href="/asset/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">	
 	<link href="/asset/lib/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
 </head>
 <body>
-	<!-- 모달 -->
-	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 	
+	<!--네비 -->
+	<div class="navbar">
+		<div class="navbar-inner">
+			<div class="container">
+				<!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</a>
+				<!-- Be sure to leave the brand out there if you want it shown -->
+				<a class="brand" href="#">뉴스 토론 게시판</a>
+				<!-- Everything you want hidden at 940px or less, place within here -->
+				<div class="nav-collapse collapse">
+					<!-- .nav, .navbar-search, .navbar-form, etc -->
+					<ul class="nav nav-pills pull-right">
+						 <em><strong><?php echo "님 환영합니다" ?></strong></em>
+						 &nbsp;&nbsp;&nbsp;
+						<button class="btn btn-primary" >로그아웃</button>
+					</ul>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- 메인 그리드 -->
+	<div class="container-fluid" style="position:relative;">
